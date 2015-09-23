a3bcf51955c91777a7b3b757bb5a99d200115f87
Lauma <lauma.pretkalnina@gmail.com>
2015-09-23 13:54:49 +0300

    Netaisīt šķirkļus no tukšām rindām; risinājums "NG. teksts" un "@5;" problēmām.

--- src/lv/ailab/tezaurs/DictionaryToDicUI.java
+++ src/lv/ailab/tezaurs/DictionaryToDicUI.java
@@ -5,7 +5,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import lv.ailab.tezaurs.checker.Markers;
 import lv.ailab.tezaurs.io.DocLoader;
 
 public class DictionaryToDicUI
@@ -17,7 +20,7 @@ public class DictionaryToDicUI
 			"\\s(?=IN\\s([^I]|I[^N]|IN[^\\s]))"; // Otrais gadījums īpaši šķirklim Indija.
 	protected static String[][] cleanupPatterns = {
 			{"(?<=[(\\[])\\s*(@5|@2)\\s", ""},
-			{"\\s(@5|@2)\\s*(?=[,.?!)\\]])", ""},
+			{"\\s(@5|@2)\\s*(?=[;,.?!)\\]])", ""},
 			{"\\s(@5|@2)\\s", " "},
 			{"[\u2013\u2014]", "-"},	// Aizstāj n-dash un m-dash ar parasto defisi u002D
 			{"[\u2018\u2019]", "'"},	// Aizstāj ar apastrofu vienpēdiņas
@@ -61,22 +64,30 @@ public class DictionaryToDicUI
 			System.out.println(fileName + " [Pabeigts]");
 			
 		}
-		System.out.println("Viss pabeigts!"); // paziņujoms par visu failu pabeigšanu
+		System.out.println("Viss pabeigts!"); // paziņojums par visu failu pabeigšanu
 	}
 	
 
 	/**
 	 * Sadala šķirkli elementos un veic citas normalizācijas.
+	 * Tukšām rindām atgriež tukšu masīvu.
 	 */
 	protected static String[] convertEntry(String entry)
 	{
-		//entry = "VR " + entry.trim();
-		for (String[] replPat : cleanupPatterns)
-			entry = entry.replaceAll(replPat[0], replPat[1]);
+		if (entry.trim().length() < 1) return new String[] {};
+		String[] rows = entry.split(splitPattern);
+		for (int i = 0; i < rows.length; i++)
+		{
+			for (String[] replPat : cleanupPatterns)
+				rows[i] = rows[i].replaceAll(replPat[0], replPat[1]);
+			// Lai nesanāk tā, ka pēc tīrīšanas aiz marķiera vairs nav atstarpes.
+			Matcher securityCheck = Pattern.compile(Markers.regexp + "([^ ].*)").matcher(rows[i]);
+			if (securityCheck.matches())
+				rows[i] = securityCheck.group(1) + " " + securityCheck.group(2);
+		}
 
 		// Te tiek nodrošināts, ka, ja šķirkļa vārds sakrīt ar kādu no
 		// marķieriem, tas netiek atdalīts atsevšķā rindā no VR.
-		String[] rows = entry.split(splitPattern);
 		if (rows.length > 0)
 			rows[0] = "VR " + rows[0];
 		return rows;
@@ -89,14 +100,17 @@ public class DictionaryToDicUI
 	protected static void printEntry(String[] entryElements, BufferedWriter out)
 			throws IOException
 	{
+		int rows = 0;
 		for (String elem : entryElements)
 		{
 			elem = elem.trim();
+			if (elem.length() < 1) continue;
 			if (elem.length() == 2) elem = elem + " "; // Jo tukšiem elementiem aiz elementa idenentifikatora jāseko vienai atstarpei.
 			out.write(elem);
 			out.newLine();
+			rows++;
 		}
-		out.newLine();
+		if (rows > 0) out.newLine();
 		out.flush();
 	}
 }

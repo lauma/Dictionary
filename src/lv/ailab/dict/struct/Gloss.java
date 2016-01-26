86d6e57d27fa2a33404311801a8cb25b9e21aa48
Lauma <lauma.pretkalnina@gmail.com>
2016-01-26 15:48:29 +0200

    Restrukturizācija - universālo vārdnīcas struktūru atdalīsana no Tēzauram specifiskajām.

--- src/lv/ailab/dict/struct/Gloss.java
+++ src/lv/ailab/dict/struct/Gloss.java
@@ -1,38 +1,44 @@
 /*******************************************************************************
  * Copyright 2013-2016 Institute of Mathematics and Computer Science, University of Latvia
  * Author: Lauma Pretkalniņa
- * 
+ *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
- * 
+ *
  *     This program is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
- * 
+ *
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
-package lv.ailab.dict.tezaurs.analyzer.struct;
+package lv.ailab.dict.struct;
 
-import lv.ailab.dict.struct.Gloss;
-import org.w3c.dom.Node;
+import lv.ailab.dict.utils.HasToJSON;
+
+import org.json.simple.JSONObject;
 
 /**
- * d (definīcijas) lauks Tēzaura XML
+ * Vārda skaidrojums (šobrīd tikai teksts).
+ * @author Lauma
  */
-public class TGloss extends Gloss
+public class Gloss implements HasToJSON
 {
-	
-	public TGloss(Node dNode)
+	/**
+	 * Definīcijas teksts
+	 */
+	public String text = null;
+
+	public Gloss(String text)
 	{
-		super(dNode.getTextContent());
-	}
-	public TGloss(String text)
-	{
-		super(text);
+		this.text = text;
 	}
 
+	public String toJSON()
+	{
+		return String.format("\"Gloss\":\"%s\"", JSONObject.escape(text));
+	}
 }

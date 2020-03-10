4ef806251e23972f0c3907d79509a9d66dc0d4ad
Lauma <lauma.pretkalnina@gmail.com>
2020-03-10 18:36:57 +0200

    Tēzaura dīvainās konstantes atdalītas no vispārējā.

--- src/lv/ailab/dict/struct/constants/structrestrs/Frequency.java
+++ src/lv/ailab/dict/struct/constants/structrestrs/Frequency.java
@@ -8,7 +8,6 @@ public class Frequency
 	public static String ONLY = "Tikai";
 	public static String USUALLY = "Parasti";
 	public static String OFTEN = "Bieži";
-	public static String ALSO = "Arī"; // TODO - tikt vaļā no šī.
 	public static String RARE = "Reti";
 	public static String UNDISCLOSED = null;
 }
---
+++ src/lv/ailab/dict/tezaurs/struct/constants/structrestrs/TFrequency.java
@@ -0,0 +1,10 @@
+package lv.ailab.dict.tezaurs.struct.constants.structrestrs;
+
+import lv.ailab.dict.struct.constants.structrestrs.Frequency;
+
+public class TFrequency extends Frequency
+{
+	public static String ALSO = "Arī"; // TODO - tikt vaļā no šī.
+	public static String RARER = "Retāk"; // TODO - tikt vaļā no šī.
+
+}
